5629d0dc5b8e9784897a2270732cc64a987060bf
Julia Wiater <juliaw@student.agh.edu.pl>
2024-01-18 14:25:26 +0100

    poprawione

--- servers.py
+++ servers.py
@@ -1,95 +1,102 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-from typing import Optional, List
-from abc import ABC, abstractmethod
+    
+from typing import Optional, List, Union, TypeVar
 import re
 
-class Product:
-    # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą argumenty wyrażające nazwę produktu (typu str) i jego cenę (typu float) -- w takiej kolejności -- i ustawiającą atrybuty `name` (typu str) oraz `price` (typu float)
+from abc import abstractmethod, ABC
 
-    def __init__(self, name: str, price: int | float) -> None:
-        if not isinstance(name, str) or not isinstance(price, (int, float)):
-            raise ValueError
-        elif re.fullmatch(r"\b[a-zA-Z]+[0-9]+\b", name) is None:
-            raise ValueError
+
+class Product:
+    def __init__(self, name: str, price: Union[float, int]) -> None:
+        if not (isinstance(name, str) and isinstance(price, (int, float))):
+            raise ValueError("The name or price is wrong type!")
+        elif not re.findall(r'^[A-Za-z]+\d+$', name):
+            raise ValueError("The name have bad pattern!")
+        elif price <= 0:
+            raise ValueError("The price is bad!")
         else:
             self.name = name
             self.price = price
 
-    def get_price(self) -> float:
-        return self.price
-
-    def get_name(self) -> str:
-        return self.name
-
-    def __eq__(self, other):
-        return self.price == other.price and self.name == other.name  # FIXME: zwróć odpowiednią wartość
+    def __eq__(self, other: "Product"):
+        return self.__hash__() == other.__hash__()
 
     def __hash__(self):
         return hash((self.name, self.price))
 
 
-class TooManyProductsFoundError(Exception):
-    # Reprezentuje wyjątek związany ze znalezieniem zbyt dużej liczby produktów.
-    def __init__(self, msg=None):
-        if msg is None:
-        # Ustaw domys´lny uz˙yteczny (!) komunikat
-            msg = f"Too many products"
-        super().__init__(msg) # wywołanie konstruktora klasy 'Exception'
+class ServerError(Exception):
+    pass  
 
-class Serwer(ABC):
+
+class TooManyProductsFoundError(ServerError):
+    pass
+    
+    
+class Server(ABC):
+    n_max_returned_entries = 3
 
     @abstractmethod
-    def get_products(self) -> List[Product]:
-        pass
+    def __init__(self, products: List[Product]) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_entries_(self, n_letters: int) -> List[Product]:
+        raise NotImplementedError
+    
+    def get_entries(self, n_letters: int) -> List[Product]:
+        entries = self.get_entries_(n_letters)
+        if entries is None:
+            return []
+        elif Server.has_too_many_products(entries):
+            raise TooManyProductsFoundError(f"Liczba znalezionych produktów przekracza maks. wartość: {len(entries)} > {Server.n_max_returned_entries}")
+        else:
+            return sorted(entries, key=lambda p: p.price)
+    
+    @staticmethod
+    def match_product_name(product: Product, n_letters: int) -> Union[re.Match, None]:
+        return re.fullmatch(f'^[a-zA-Z]{{{n_letters}}}\\d{{2,3}}$', product.name)
+
+    @staticmethod
+    def has_too_many_products(entries: List[Product]) -> bool:
+        return len(entries) > Server.n_max_returned_entries
 
 
-# FIXME: Każada z poniższych klas serwerów powinna posiadać:
-#   (1) metodę inicjalizacyjną przyjmującą listę obiektów typu `Product` i ustawiającą atrybut `products` zgodnie z typem reprezentacji produktów na danym serwerze,
-#   (2) możliwość odwołania się do atrybutu klasowego `n_max_returned_entries` (typu int) wyrażający maksymalną dopuszczalną liczbę wyników wyszukiwania,
-#   (3) możliwość odwołania się do metody `get_entries(self, n_letters)` zwracającą listę produktów spełniających kryterium wyszukiwania
-
-class ListServer:
-    n_max_returned_entries = 5
-
-    def __init__(self, product_list: List[Product]) -> None:
-        self.products = product_list
-
-    def get_entries(self,n_letters=1) -> List[Product]:
-        ret_list = [prod for prod in self.products if re.fullmatch('^[a-zA-Z]{{{n}}}\\d{{2,3}}$'.format(n=n_letters), prod.get_name())]
-        if len(ret_list) > self.n_max_returned_entries:
-            raise TooManyProductsFoundError
-        return sorted(ret_list, key=lambda prod: prod.get_price())
+ServerType = TypeVar("ServerType", bound=Server)
 
 
+class ListServer(Server):
+    def __init__(self, products: List[Product]) -> None:
+        self.products_ = products
 
-class MapServer:
-    n_max_returned_entries = 5
+    def get_entries_(self, n_letters: int) -> List[Product]:
+        return [p for p in self.products_ if self.match_product_name(p, n_letters)]
+    
+    
+class MapServer(Server):
+    def __init__(self, products: List[Product]) -> None:
+        self.products_ = {p.name:p for p in products}
 
-    def __init__(self, product_list: List[Product]) -> None:
-        self.products = {elem.get_name():elem.get_price() for elem in product_list}
-
-    def get_entries(self,n_letters=1) -> List[Product]:
-        ret_list = [Product(name, price) for name, price in self.products.items() if re.fullmatch('^[a-zA-Z]{{{n}}}\\d{{2,3}}$'.format(n=n_letters), name)]
-        if len(ret_list) > self.n_max_returned_entries:
-            raise TooManyProductsFoundError
-        return sorted(ret_list, key=lambda prod: prod.get_price())
+    def get_entries_(self, n_letters: int) -> [Product]:
+        return [p[1] for p in self.products_.items() if self.match_product_name(p[1], n_letters)]
 
 
 class Client:
     # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą obiekt reprezentujący serwer
-
-    def __init__(self, server: ListServer | MapServer) -> None:
+    
+    def __init__(self, server: ServerType):
         self.server = server
 
     def get_total_price(self, n_letters: Optional[int]) -> Optional[float]:
-       try:
-            list_of_entries = [prod.get_price() for prod in self.server.get_entries(n_letters)]
-            if not list_of_entries:
-                raise Exception("Empty list")
-            else:
-             return sum(list_of_entries)
-       except:
-           return None
-
+        sum_ = 0
+        try:
+            entries = self.server.get_entries(n_letters)
+            if not entries:
+                raise Exception("a list is empty")
+            for e in entries:
+                sum_ += e.price
+        except:
+            return None
+        else:
+            return sum_
